733bc4ee7910727212303d03a9dc6114d9474b64
PMKnapp001 <pmknapp001@outlook.com>
2022-11-12 19:12:52 -0500

    assignment completed

--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,24 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
+from random import choice
+import customers
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+def pick_winner(pool):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = choice(pool)
+
+    name = chosen_customer.name
+    email = chosen_customer.email
+
+    print(f"Tell {name} at {email} that they've won")
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    pool = customers.get_customers_from_file("customers.txt")
+    pick_winner(pool)
+
+print("V2")
+run_raffle()
